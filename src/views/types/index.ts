c15bb767bda6c807d3d73debe16d23f59c6767e1
qiux <qiux@damddos.com>
2025-03-27 11:06:47 +0800

    fix: 枚举值改为字符串

--- src/views/types/index.ts
+++ src/views/types/index.ts
@@ -7,29 +7,29 @@ export enum LOGIC_TYPE_ENUM {
 // 操作符枚举
 export enum OPERATOR_ENUM {
   // 等于
-  EQUAL = 1,
+  EQUAL = '1',
   // 不等于
-  NOT_EQUAL = 2,
+  NOT_EQUAL = '2',
   // 大于
-  GREATER_THAN = 3,
+  GREATER_THAN = '3',
   // 小于
-  LESS_THAN = 4,
+  LESS_THAN = '4',
   // null
-  IS_NULL = 5,
+  IS_NULL = '5',
   // in
-  IN = 6,
+  IN = '6',
   // 大于等于
-  GREATER_THAN_OR_EQUAL = 7,
+  GREATER_THAN_OR_EQUAL = '7',
   // 小于等于
-  LESS_THAN_OR_EQUAL = 8,
+  LESS_THAN_OR_EQUAL = '8',
   // regexp
-  REGEXP = 9,
+  REGEXP = '9',
   // like
-  LIKE = 10,
+  LIKE = '10',
   // isEmpty
-  IS_EMPTY = 11,
+  IS_EMPTY = '11',
   // contains
-  CONTAINS = 12,
+  CONTAINS = '12',
 }
 
 // 操作类型枚举
